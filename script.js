5f3542ece3a24cfb8bd73585f085fbd5dd990860
Lawrence <lawrencehome21@gmail.com>
2023-06-25 05:52:06 -0700

    created some js variables

---
+++ script.js
@@ -0,0 +1,10 @@
+const accessKey = "8K-N3iaX_GgggjKQAHday5j2QQ9vxKE5LqImKuqfoJ4";
+
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
+
